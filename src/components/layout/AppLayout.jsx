d81d43223e0461149cc085a868158344c817cff9
Penerobos Daerah <penerobos.daerah.aunz@gmail.com>
2026-06-06 15:19:49 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -4,16 +4,18 @@ import MobileNav from "./MobileNav";
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen bg-background md:flex">
-      <div className="hidden md:block md:fixed md:left-0 md:top-0 md:h-screen md:w-64 md:overflow-y-auto">
-        <Sidebar />
-      </div>
-      <main className="md:ml-64 pb-24 md:pb-8">
-        <div className="p-3 md:p-8">
-          <Outlet />
+    <>
+      <div className="min-h-screen bg-background md:flex">
+        <div className="hidden md:block md:fixed md:left-0 md:top-0 md:h-screen md:w-64 md:overflow-y-auto">
+          <Sidebar />
         </div>
-      </main>
+        <main className="w-full md:ml-64 pb-24 md:pb-8">
+          <div className="p-3 md:p-8">
+            <Outlet />
+          </div>
+        </main>
+      </div>
       <MobileNav />
-    </div>
+    </>
   );
 }
