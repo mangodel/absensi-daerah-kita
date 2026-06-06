9e7794d6e270c18338575d9ee4b794f11b43159a
Penerobos Daerah <penerobos.daerah.aunz@gmail.com>
2026-06-06 14:55:37 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import MobileNav from "./MobileNav";
-import MobileNavTest from "./MobileNavTest";
 
 export default function AppLayout() {
   return (
@@ -9,12 +8,12 @@ export default function AppLayout() {
       <div className="hidden md:block">
         <Sidebar />
       </div>
-      <main className="md:ml-64 flex-1 min-h-screen md:pb-0 pb-20">
-        <div className="p-3 md:p-8">
+      <main className="md:ml-64 flex-1 min-h-screen md:pb-0">
+        <div className="p-3 md:p-8 pb-24">
           <Outlet />
         </div>
       </main>
-      <MobileNavTest />
+      <MobileNav />
     </div>
   );
 }
--- src/components/layout/MobileNavTest.jsx
+++
@@ -1,7 +0,0 @@
-export default function MobileNavTest() {
-  return (
-    <div className="md:hidden fixed bottom-0 left-0 right-0 bg-red-500 h-32 z-50 w-full flex items-center justify-center">
-      <div className="text-white text-2xl font-bold">MOBILE NAV</div>
-    </div>
-  );
-}
