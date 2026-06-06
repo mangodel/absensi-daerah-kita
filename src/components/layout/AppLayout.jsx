84fc2ba62e4e0b96fd617acb4ffe21ae06d2c10b
Penerobos Daerah <penerobos.daerah.aunz@gmail.com>
2026-06-06 14:39:34 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -4,16 +4,16 @@ import MobileNav from "./MobileNav";
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen bg-background flex flex-col">
+    <div className="min-h-screen bg-background">
       <div className="hidden md:block">
         <Sidebar />
       </div>
-      <main className="md:ml-64 flex-1 min-h-screen">
-        <div className="p-3 md:p-8 pb-24 md:pb-8">
+      <main className="md:ml-64 min-h-screen pb-24 md:pb-8">
+        <div className="p-3 md:p-8">
           <Outlet />
         </div>
       </main>
-      <div className="md:hidden fixed bottom-0 left-0 right-0 z-50">
+      <div className="md:hidden">
         <MobileNav />
       </div>
     </div>
